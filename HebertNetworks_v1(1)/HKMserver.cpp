5436cd5099f70634be56e5ca4c3285944767536f
wouldn't you <liketoknow@gmail.gtfo>
2022-11-15 13:40:05 -0600

    updated some line spacing

--- HebertNetworks_v1(1)/HKMserver.cpp
+++ HebertNetworks_v1(1)/HKMserver.cpp
@@ -253,12 +253,7 @@ int main(int argc, char const *argv[]) {
                                                         windowUpperBound ++;
                                                 }
 
-
-
-                                                
                                         }
-
-                                        
 
                                 }
 
