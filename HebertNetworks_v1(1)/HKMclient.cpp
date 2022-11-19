494ffd4bf41b80ae6aa28917322ecb89ae00290d
Oliver Mahan <mahano0661@uwec.edu>
2022-11-19 16:18:48 -0600

    removed unused code

--- HebertNetworks_v1(1)/HKMclient.cpp
+++ HebertNetworks_v1(1)/HKMclient.cpp
@@ -15,23 +15,6 @@
 
 using namespace std;
 
-//int portNumber;
-//string IPaddress;
-//int sock;               
-int valueRead;          // what is this variable?
-//int packetSize;
-//int windowSize;
-//int seqrange;
-//int nextSequenceNum = 0;
-//string line;
-//string reset;
-//string fileName;
-//string payOut = "";
-//string fileInput;
-//FILE * pFile;
-//string empty = "\n";
-
-
 typedef struct Packet{
         public:
                 uint32_t packetID;
@@ -47,31 +30,6 @@ string UserInputPromptAddr()
         cin >> ip;
         return ip;	
 }
-
-/*
-//prompts te user for the port number
-int UserInputPromptPort()
-{
-        int port;
-        cout << "Enter port number or -1 for default (" << PORT_NUMBER << "): ";
-        cin >> port;
-        if (port < 0)
-        {
-                port = PORT_NUMBER;
-        }
-        
-        return port;
-}
-
-//returns name of input file.
-string UserInputPromptFile()
-{
-        string file;
-        cout << "Enter the name of your file: ";
-        cin >> file;
-        return file;
-}
-*/
 
 //returns packet size from user input
 int UserInputPromptPacket()
@@ -210,127 +168,53 @@ int main(int argc, char const *argv[]) {
         header[0] = packetSize;
         header[1] = windowSize;
         header[2] = sequenceNumSize;
-
-        pollfd pfd;
-        pfd.fd = sock;
-        pfd.events = POLLIN;
-        int rc;
-        int timeout = -1; // still need to implement this, but -1 means it blocks until the event occurs
-        
         
         bool ackRecv = false;
         char ackHeaderRecv;// = new int[1]();
 
-    //    while (ackHeaderRecv[0] != 1) // while we haven't gotten a successful ack from server
-    //    {
-        
-                send(sock, header, HEADER_SIZE, 0);
+        send(sock, header, HEADER_SIZE, 0);
 
-                if (USE_POLL)
-                {
-                                        
-                        rc = poll(&pfd, 1, timeout);
-
-                        if (rc < 0)
-                        {
-                                printf("poll error");
-                                exit(1);
-                        }
-
-                        if (rc == 0)
-                        {
-                                printf("poll timed out");
-                                exit(0);
-                        }
-                }
-
-                // only get here if poll() found something to read from the socket
-                // server sends back 1 for successful reception of header information
-                recv(sock, &ackHeaderRecv, sizeof(ackHeaderRecv), 0);
-     //   }
-
-/* old method for sending header and receiving ack
-        while (ackHeaderRecv[0] <= 0) 
+        // server sends back 1 for successful reception of header information        
+        recv(sock, &ackHeaderRecv, sizeof(ackHeaderRecv), 0);
+        if (ackHeaderRecv != '1')
         {
-                while (ackHeaderRecv[0] == 0) 
-                {
-
-                send(sock, &header, sizeof(header), 0);
-
-                // ioctl makes sure there is information to read. Stores bytes to read in checkStatus
-                ioctl(sock, FIONREAD, &checkStatus);
-                int *ackHeaderRecv = {0};
-                if (checkStatus > 0)
-                {
-                // server sends back 1 for successful reception of header information
-                        recv(sock, ackHeaderRecv, sizeof(ackHeaderRecv), 0);
-                        // if ackHeaderRecv returns -1, print an invalid connection & close ( I think this would go here?)
-                        if (ackHeaderRecv[0] == -1) 
-                        {
-                        perror("invalid connection");
-                        exit(1);
-                        }
-                // TODO: 
-                //        Timeout implementation here at some point
-                //        have server receive this header and send the ack 
-                }
-                }
+                printf("\nHeader ack not received\n");
+                return -1;
         }
-        //send header here *************
-*/
         
-        // the server got the header, so we are good to continue sending the file.
-        
-                //creates a file to print to?
-                //FILE *pFile = fopen(fileName.c_str(), "r");
-                
-                //creates an ifstream named "readStream" that reads from the user's selected file
-                ifstream readStream(fileName);
-                char placeHolder;
-                
-                //ostringstream temp;
-                
-                string fileInput;
+        //creates an ifstream named "readStream" that reads from the user's selected file
+        ifstream readStream(fileName);
+        char placeHolder;        
+        string fileInput;
 
-                //while we haven't hit the end of the file, start sending packets.
-                while(!readStream.eof()){
+        //while we haven't hit the end of the file, start sending packets.
+        while(!readStream.eof()){
 
-                        fileInput.clear();
-                        //payOut.clear();
+                fileInput.clear();
+                //payOut.clear();
 
-                        //pull characters from readStream until fileInput == one packet.
-                        for (int i = 0; i < packetSize && readStream.get(placeHolder); i++){
-                                fileInput.push_back(placeHolder);
-                        }
-
-                        int nextSequenceNum = 0;
-                        //if fileInput isn't empty, we have a packet to send.
-                        //send the packet and its size to the server, and tell the user we did it.
-                        if(!fileInput.empty()){
-                                //payOut.append(fileInput);
-                                nextSequenceNum++;
-                                int bufsize = fileInput.length();
-                                send(sock, &bufsize, sizeof(bufsize), 0);
-                                send(sock, fileInput.data(), fileInput.size(), 0);
-                                cout << "Packet " << nextSequenceNum << " sent: " << endl;
-                        }
-
+                //pull characters from readStream until fileInput == one packet.
+                for (int i = 0; i < packetSize && readStream.get(placeHolder); i++){
+                        fileInput.push_back(placeHolder);
                 }
 
-                //we're done sending packets. finish everything up.
-                cout << "Packets sent. Complete"<< endl;
-                string verify = "md5sum " + fileName;
-                system(verify.c_str());
-        
-        // server never acked the header, so we don't know that the connection is solid.
-        /*else {
-                cout << "Couldn't verify header info with server, exiting now." << endl;
-                return 0;
-        }*/
-        
-        
+                int nextSequenceNum = 0;
+                //if fileInput isn't empty, we have a packet to send.
+                //send the packet and its size to the server, and tell the user we did it.
+                if(!fileInput.empty()){
+                        //payOut.append(fileInput);
+                        nextSequenceNum++;
+                        int bufsize = fileInput.length();
+                        send(sock, &bufsize, sizeof(bufsize), 0);
+                        send(sock, fileInput.data(), fileInput.size(), 0);
+                        cout << "Packet " << nextSequenceNum << " sent: " << endl;
+                }
 
-        
-        
+        }
 
+        //we're done sending packets. finish everything up.
+        cout << "Packets sent. Complete"<< endl;
+        string verify = "md5sum " + fileName;
+        system(verify.c_str());
+                
 }
