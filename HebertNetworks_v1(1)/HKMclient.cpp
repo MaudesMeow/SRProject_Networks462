2ea36575f07c6a1614bfad912dca7e2581a2fc86
Jordan Hebert <87828418+MaudesMeow@users.noreply.github.com>
2022-11-22 21:25:46 -0600

    window lower bound update

--- HebertNetworks_v1(1)/HKMclient.cpp
+++ HebertNetworks_v1(1)/HKMclient.cpp
@@ -494,15 +494,15 @@ int main(int argc, char const *argv[]) {
                                 srpBuffer[ackReceived].isFull = false;
 
                                 //slide the window
-                                windowLowerBound ++;
-                                windowUpperBound ++;
+                                windowLowerBound = (windowLowerBound +1) % (sequenceNumSize +1);
+                                windowUpperBound = (windowUpperBound +1) % (sequenceNumSize +1);
 
                                 //make sure we move the window to compensate for all other previously recieved acks for packets that we've recorded.
                                 while(srpBuffer[windowLowerBound].isAcked && srpBuffer[windowLowerBound].isFull){
 
                                         srpBuffer[windowLowerBound].isFull = false;
-                                        windowLowerBound ++;
-                                        windowUpperBound ++;
+                                        windowLowerBound = (windowLowerBound +1) % (sequenceNumSize +1);
+                                        windowUpperBound = (windowUpperBound +1) % (sequenceNumSize +1);
                                         
                                 }
 
